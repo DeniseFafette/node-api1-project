6a7bfe01fd8a283761cd812d2f40b4bdf93ee50f
DeniseFafette <denisefafette@gmail.com>
2020-01-18 13:18:19 -0800

    updated server.post

--- index.js
+++ index.js
@@ -6,11 +6,13 @@
 // require express and import the express package
 // import database
 // create the server
+// tell express to interpret the type of string with middleware
 // set server to listen on a port - why is {} uses/not used?
 
 const express = require('express'); 
 const db = require('./data/db.js'); 
 const server = express(); 
+server.use(express.json());
 server.listen(3000, () => console.log('server is listening on port 3000'));
 
 
@@ -20,29 +22,56 @@ server.listen(3000, () => console.log('server is listening on port 3000'));
 
 server.get('/api/users', (req, res) => {
     db.find()
-        .then(users => res.status(200).json(users));
-});
-
-server.get('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    db.findById(id)
-        .then(user => res.status(200).json(user));
-    //console.log(req.params);
+        .then(users => res.status(200).json(users))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: 'The users information could not be retrieved'});
+        });
 });
 
 server.post('/api/users', (req, res) => {
-    db.insert(req.body)
-    .then(data => {
-        res.status(201).json(data)
+    console.log(req.body);
+    const user = req.body;
+    db.insert(user)
+    .then(({ id }) => {
+        db.findById(id)
+        .then(user => {
+        res.status(201).json(user);
     })
     .catch(err => {
-        console.log(err)
-    })
+        console.log(err);
+        res.status(500).json({error: 'Server error retrieving user'});
+    });
   })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json({error: 'Server error adding user'});
+  });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            console.log('user', user);
+            if (user) {
+                res.status(200).json(user);
+            } else {
+            res.status(404).json({error: 'The user with the specified ID does not exist.'});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({errror: 'The user information could not be retrieved.'});
+        });
+
+});
+
+
   
 
 
-//server.use(express.json())
+
 
 
 
